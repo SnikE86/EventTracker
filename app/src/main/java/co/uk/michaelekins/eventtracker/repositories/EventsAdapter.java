d709b79b05ff885b68d86317bb55c013c8bb24a6
CW-DATA\michael ekins <m.ekins@craneware.com>
2020-04-08 20:09:31 +0100

    Removed comments

--- app/src/main/java/co/uk/michaelekins/eventtracker/repositories/EventsAdapter.java
+++ app/src/main/java/co/uk/michaelekins/eventtracker/repositories/EventsAdapter.java
@@ -5,24 +5,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.LiveData;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.LinkedList;
 
 import co.uk.michaelekins.eventtracker.R;
 import co.uk.michaelekins.eventtracker.events.InstanceEvent;
-import co.uk.michaelekins.eventtracker.ui.viewevents.EventSummaryFragment;
 
 public class EventsAdapter  extends RecyclerView.Adapter<EventsAdapter.MyViewHolder> {
     private LinkedList<InstanceEvent> mDataset;
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
         public View view;
         public MyViewHolder(View aview) {
             super(aview);
@@ -30,7 +23,6 @@ public class EventsAdapter  extends RecyclerView.Adapter<EventsAdapter.MyViewHol
         }
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
     public EventsAdapter() {
         mDataset = new LinkedList<InstanceEvent>();
     }
@@ -39,35 +31,27 @@ public class EventsAdapter  extends RecyclerView.Adapter<EventsAdapter.MyViewHol
         mDataset.add(newInstanceEvent);
     }
 
-    // Create new views (invoked by the layout manager)
     @Override
     public EventsAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
                                                      int viewType) {
-        // create a new view
         View aview = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.fragment_event_summary, parent, false);
 
-        MyViewHolder vh = new MyViewHolder(aview);
-        return vh;
+        MyViewHolder viewholder = new MyViewHolder(aview);
+        return viewholder;
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
         TextView name = holder.view.findViewById((R.id.text_event_summary_name));
         TextView date = holder.view.findViewById((R.id.text_event_summary_date));
         TextView tags = holder.view.findViewById((R.id.text_event_summary_tags));
-
-        InstanceEvent event = mDataset.get(position);
 
         name.setText("Name: " + mDataset.get(position).name);
         //date.setText(mDataset.get(position).startDate.toString());
         //tags.setText(mDataset.get(position).tags.toString());
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return mDataset.size();
